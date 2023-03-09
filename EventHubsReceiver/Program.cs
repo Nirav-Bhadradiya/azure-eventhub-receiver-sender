c9920348543783a3a638ef4b36b1556bd7f190a6
Nirav Bhadradiya <nirav.bhadradiya@nl.abnamro.com>
2023-03-09 20:50:56 +0100

    adding to github

---
+++ EventHubsReceiver/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+using LogAnalytics.Client;
+
+namespace EventHubsReceiver
+{
+    class Program
+    {
+        static BlobContainerClient storageClient;
+
+        // The Event Hubs client types are safe to cache and use as a singleton for the lifetime
+        // of the application, which is best practice when events are being published or read regularly.        
+        static EventProcessorClient processor;
+        static async Task Main()
+        {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .AddUserSecrets<Program>()
+            .Build();
+
+#if DEBUG
+            //create proxy to make eventhub calls from local machine
+            ICredentials credentials = CredentialCache.DefaultCredentials;
+            IWebProxy proxy = new WebProxy(config.GetSection("ProxyHost").Value, 9000)
+            {
+                Credentials = credentials
+            };
+            //use AMQP WEbscokets transport to avoid connection issue from local machine, 
+            // AMQPWebSocket uses TCP/443 to connect to eventhub instead port 5671,5672
+            EventHubConnectionOptions connectionOptions = new EventHubConnectionOptions
+            {
+                TransportType = EventHubsTransportType.AmqpWebSockets,
+                Proxy = proxy
+            };
+
+            EventProcessorClientOptions eventProcessorClientOptions = new EventProcessorClientOptions
+            {
+                ConnectionOptions = connectionOptions
+            };
+#endif
+
+#if DEBUG
+            EventHubConnection connection = new EventHubConnection(config.GetConnectionString("EventHub"), config.GetSection("EventHubName").Value, connectionOptions);
+#else
+            EventHubConnection connection = new EventHubConnection(config.GetConnectionString("EventHub"),config.GetSection("EventHubName").Value);
+#endif
+
+            // Read from the default consumer group: $Default
+            string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+            // Create a blob container client that the event processor will use 
+            storageClient = new BlobContainerClient(config.GetConnectionString("StorageAccount"), config.GetSection("BlobContainerName").Value);
+
+#if DEBUG
+            // Create an event processor client to process events in the event hub
+            processor = new EventProcessorClient(storageClient, consumerGroup, config.GetConnectionString("EventHub"), config.GetSection("EventHubName").Value, eventProcessorClientOptions);
+#else
+            processor = new EventProcessorClient(storageClient, consumerGroup, config.GetConnectionString("EventHub"), config.GetSection("EventHubName").Value);
+#endif
+            // Register handlers for processing events and handling errors
+            processor.ProcessEventAsync += ProcessEventHandler;
+            processor.ProcessErrorAsync += ProcessErrorHandler;
+
+            // Start the processing
+            await processor.StartProcessingAsync();
+
+            // Wait for 30 seconds for the events to be processed
+            await Task.Delay(TimeSpan.FromSeconds(6000));
+
+            // Stop the processing
+            await processor.StopProcessingAsync();
+        }
+
+        static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+        {
+            // Write the body of the event to the console window
+            Console.WriteLine("\tReceived event: {0} With Properties : {1}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()), string.Join(Environment.NewLine, eventArgs.Data.Properties));
+
+            //push to Log Analytics
+            //LogAnalyticsClient logAnalytics = new LogAnalyticsClient(config.GetSection("EventHubName").Value, config.GetSection("EventHubName").Value);
+
+            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+            await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+        }
+
+        static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+        {
+            // Write details about the error to the console window
+            Console.WriteLine($"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+            Console.WriteLine(eventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ EventHubsSender/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+
+namespace EventHubsSender
+{
+    class Program
+    {
+        // number of events to be sent to the event hub
+        private const int numOfEvents = 300;
+
+        // The Event Hubs client types are safe to cache and use as a singleton for the lifetime
+        // of the application, which is best practice when events are being published or read regularly.
+        static EventHubProducerClient producerClient;
+
+        static async Task Main()
+        {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .AddUserSecrets<Program>()
+            .Build();
+#if DEBUG
+            //create proxy to make eventhub calls from local machine
+            ICredentials credentials = CredentialCache.DefaultCredentials;
+            IWebProxy proxy = new WebProxy(config.GetSection("ProxyHost").Value, 9000)
+            {
+                Credentials = credentials
+            };
+            //use AMQP WEbscokets transport to avoid connection issue from local machine, 
+            // AMQPWebSocket uses TCP/443 to connect to eventhub instead port 5671,5672
+            EventHubConnectionOptions connectionOptions = new EventHubConnectionOptions
+            {
+                TransportType = EventHubsTransportType.AmqpWebSockets,
+                Proxy = proxy
+            };
+#endif
+
+#if DEBUG
+            EventHubConnection connection = new EventHubConnection(config.GetConnectionString("EventHub"),config.GetSection("EventHubName").Value, connectionOptions);
+#else
+            EventHubConnection connection = new EventHubConnection(config.GetConnectionString("EventHub"),config.GetSection("EventHubName").Value);
+#endif
+            // Create a producer client that you can use to send events to an event hub
+            producerClient = new EventHubProducerClient(connection);
+
+            // Create a batch of events 
+            using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+            for (int i = 1; i <= numOfEvents; i++)
+            {
+                var eventBody = new BinaryData("Hello, Event Hubs!");
+                var eventData = new EventData(eventBody);
+                eventData.Properties.Add("EventType", "com.microsoft.samples.hello-event");
+                eventData.Properties.Add("priority", 1);
+                eventData.Properties.Add("score", 9.0);
+                eventData.Properties.Add("MessageId", i);
+
+                if (!eventBatch.TryAdd(eventData))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of events to the event hub
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine($"A batch of {numOfEvents} events has been published.");
+            }
+            finally
+            {
+                await producerClient.DisposeAsync();
+            }
+        }
+    }
+}
